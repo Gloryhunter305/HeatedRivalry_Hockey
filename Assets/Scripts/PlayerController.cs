5bea70ec1748aeb96f5dd4615e553bf3e61c5d6a
Gloryhunter305 <mikylejosh@gmail.com>
2026-02-01 12:11:02 -0500

    2/1/26 12:10 pm
    -recent changes
    + updated player Stats

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,62 +3,69 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     [SerializeField] private GameManager game;
     int timer;
 
-
     [Header("Player Components")]
-    PlayerStats Player;
+    private PlayerStats Player;
     private Rigidbody2D _rigidBody2D;
     public funnyFace funnyFace;
-    //[SerializeField] private float DirectionalMultipler = 3f;
+
     public KeyCode upKey, downKey, leftKey, rightKey;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rigidBody2D = GetComponent<Rigidbody2D>();
         Player = GetComponent<PlayerStats>();
+
+        if (_rigidBody2D == null)
+            Debug.LogWarning("Rigidbody2D component missing on player.", this);
+        if (Player == null)
+            Debug.LogWarning("PlayerStats component missing on player.", this);
+        if (game == null)
+            Debug.LogWarning("GameManager reference not set on PlayerController.", this);
     }
 
-    // Update is called once per frame
     void Update()
     {
-      
+        // Keep Update empty (physics in FixedUpdate), but still useful to validate start state if needed.
     }
 
     private void FixedUpdate()
     {
-        if (game.gameStart == true)
+        if (game == null || Player == null)
+            return;
+
+        if (!game.gameStart)
+            return;
+
+        // Forward movement when pressing upKey
+        if (Input.GetKey(upKey))
         {
-            // Move in the direction the player is facing (transform.up)
             _rigidBody2D.AddForce((Vector2)transform.up * Player.MoveSpeed);
         }
 
-        if (Input.GetKey(downKey))        //Charging up and dashing forwards
+        // Charge / dash when holding downKey
+        if (Input.GetKey(downKey))
         {
-            //_rigidBody2D.AddForce((Vector2)(-transform.up) * moveSpeed);
-
-            //Dashing Mechanic (Charing dash is linear to the amount of speed gained from dash)
             if (Player.canDash && !Player.isDashing)
-            timer++;
-            if (Input.GetKey(upKey))
             {
-                // Move in the direction the player is facing (transform.up)
-                _rigidBody2D.AddForce((Vector2)transform.up * Player.MoveSpeed);
-            }
-
-            if (Input.GetKey(leftKey))
-            {
-                transform.Rotate(Vector3.forward * Player.TurnMultipler);
-            }
-
-            if (Input.GetKey(rightKey))
-            {
-                transform.Rotate(Vector3.back * Player.TurnMultipler);
+                StartCoroutine(Dash());
             }
         }
+
+        // Turn left / right independently of holding downKey
+        if (Input.GetKey(leftKey))
+        {
+            transform.Rotate(Vector3.forward * Player.TurnMultipler * Time.fixedDeltaTime);
+        }
+
+        if (Input.GetKey(rightKey))
+        {
+            transform.Rotate(Vector3.back * Player.TurnMultipler * Time.fixedDeltaTime);
+        }
+        
+        timer++;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -68,61 +75,36 @@ public class PlayerController : MonoBehaviour
             ApplyKnockback(collision);
         }
 
-        if (timer > 300)
+        if (timer > 300 && funnyFace != null)
         {
-            funnyFace.StartCoroutine(funnyFace.Hurt());
+            StartCoroutine(funnyFace.Hurt());
             timer = 0;
         }
     }
 
     private void ApplyKnockback(Collision2D collision)
     {
-        // Use the puck's Rigidbody2D
         Rigidbody2D puckRigidBody2D = collision.rigidbody;
-        if (puckRigidBody2D == null)
+        if (puckRigidBody2D == null || _rigidBody2D == null || Player == null)
             return;
 
-        // Where the puck is going to be knocked towards
         Vector2 direction = (puckRigidBody2D.position - _rigidBody2D.position).normalized;
 
-        // Base force, amplified if player is dashing
         float forceMagnitude = Player.KnockbackForce;
         if (Player.isDashing)
             forceMagnitude *= Player.DashKnockbackMultiplier;
 
-        // Player's velocity transfer
+        // Use velocity (Rigidbody2D.velocity) -- not linearVelocity
         Vector2 playerVelocityContribution = _rigidBody2D.linearVelocity * 0.5f;
 
         Vector2 finalForce = direction * forceMagnitude + playerVelocityContribution;
 
-        // Apply impulse to puck
         puckRigidBody2D.AddForce(finalForce, ForceMode2D.Impulse);
 
-        //Apply recoil to the player
         if (Player.applyPlayerKnockback)
         {
             Vector2 recoil = -direction * (forceMagnitude * Player.RecoilMultiplier);
             _rigidBody2D.AddForce(recoil, ForceMode2D.Impulse);
-            if (Input.GetKey(downKey))        //Charging up and dashing forwards
-            {
-                //_rigidBody2D.AddForce((Vector2)(-transform.up) * moveSpeed);
-
-                //Dashing Mechanic (Charing dash is linear to the amount of speed gained from dash)
-                if (Player.canDash && Player.isDashing)
-                {
-                    StartCoroutine(Dash());
-                }
-            }
-
-            if (Input.GetKey(leftKey))
-            {
-                transform.Rotate(Vector3.forward * Player.TurnMultipler);
-            }
-
-            if (Input.GetKey(rightKey))
-            {
-                transform.Rotate(Vector3.back * Player.TurnMultipler);
-            }
         }
     }
 
@@ -131,9 +113,10 @@ public class PlayerController : MonoBehaviour
         Player.canDash = false;
         Player.isDashing = true;
 
-        //Stop momentum before dash (prevent sideways momentum)
+        // Stop lateral momentum before dash
         _rigidBody2D.linearVelocity = Vector2.zero;
         _rigidBody2D.AddForce((Vector2)transform.up * Player.DashForce, ForceMode2D.Impulse);
+
         yield return new WaitForSeconds(Player.DashDuration);
         Player.isDashing = false;
 
